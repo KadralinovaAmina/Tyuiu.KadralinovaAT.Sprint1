0be8d2e2e94f03a1ff87d727bb594ae7b37a2464
Амина Кадралинова <Tyuiu.KadralinovaAT@outlook.com>
2025-09-26 20:11:27 +0500

    Add project files.

---
+++ Tyuiu.KadralinovaAT.Sprint1.Task0.V23/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.KadralinovaAT.Sprint1.Task0.V23.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #1 | Выполнила: Кадралинова А. Т. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #23                                                             *");
+Console.WriteLine("* Выполнила: Кадралинова Амина Тастемировна | ПИНб-25-1                   *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет выражение (15 / 5 * 4) + 1        *");
+Console.WriteLine("* и печатает результат на экране.                                         *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* (15 / 5 * 4) + 1                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.Calculate());
+
+Console.ReadLine();
