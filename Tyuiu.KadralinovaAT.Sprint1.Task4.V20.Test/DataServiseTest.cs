ac0c23da3c6feb1183025f7ea817b03c369ec388
Амина Кадралинова <Tyuiu.KadralinovaAT@outlook.com>
2025-09-28 23:44:41 +0500

    Task4

---
+++ Tyuiu.KadralinovaAT.Sprint1.Task4.V20.Test/DataServiseTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KadralinovaAT.Sprint1.Task4.V20.Lib;
+
+namespace Tyuiu.KadralinovaAT.Sprint1.Task4.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double wait = 1.16;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KadralinovaAT.Sprint1.Task4.V20/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KadralinovaAT.Sprint1.Task4.V20.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнила: Кадралинова А. Т. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Class Math                                                        *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #20                                                             *");
+Console.WriteLine("* Выполнила: Кадралинова Амина Тастемировна | ПИНб-25-1                   *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x, y;
+
+Console.WriteLine("Введите значение X:");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение Y:");
+y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("(1 + x) / abs(x - sqrt(2) + y) = " + ds.Calculate(x, y));
+
+Console.ReadKey();
