e6f465458f1598c409ed141eb4acf236ad1e181b
Амина Кадралинова <Tyuiu.KadralinovaAT@outlook.com>
2025-09-28 20:13:21 +0500

    Task2

---
+++ Tyuiu.KadralinovaAT.Sprint1.Task2.V10/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KadralinovaAT.Sprint1.Task2.V10.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнила: Кадралинова А. Т. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #10                                                             *");
+Console.WriteLine("* Выполнила: Кадралинова Амина Тастемировна | ПИНб-25-1                   *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчеты и печатает результат на экране.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x;
+
+Console.WriteLine("Введите значение X:");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Расстояние в дюймах X = " + ds.ConvertMetreToInchs(x));
+
+Console.ReadLine();
